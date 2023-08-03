f08bd0f3d77e7e6ebe1eec02802860f2efc809bc
Max <f233xdd@outlook.com>
2023-08-04 00:31:30 +0800

    update
    update files

--- client/host_client.py
+++ client/host_client.py
@@ -1,10 +1,28 @@
 # the part which connected with Minecraft
 import socket
 import threading
+import time
 
 import client
 
 debug: bool = False
+
+_current_time = 0
+
+
+def ticker(time_break: float) -> bool:
+    global _current_time
+
+    if _current_time == 0:
+        _current_time = time.time()
+        return False
+    else:
+        current_time_break = time.time() - _current_time
+        if current_time_break >= time_break:
+            _current_time = time.time()
+            return True
+        else:
+            return False
 
 
 class HostClient(client.Client):
@@ -14,6 +32,8 @@ class HostClient(client.Client):
 
         self._me_port = mc_port
         self._virtual_client: socket.socket
+        self.__send_func_alive = False
+        self.__get_func_alive = False
 
     def __connect_mc_server(self):
         self._virtual_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,33 +45,64 @@ class HostClient(client.Client):
 
     def __send_java_data(self):
         """send data to java"""
+        self.__send_func_alive = True
         try:
             while True:
-                data = self._data_queue_2.get()
+                try:
+                    data = self._data_queue_2.get(timeout=2)
+                except TimeoutError:
+                    if not self.__get_func_alive:
+                        self.__send_func_alive = False
+                        break
+                    else:
+                        if ticker(5):
+                            self._virtual_client.send(b'')
+                        continue
+
+                if not self.__get_func_alive:
+                    self.__send_func_alive = False
+                    break
+
                 self._virtual_client.sendall(data)
                 if debug:
                     if data:
                         print("[H]send_java_data: ", data)
         except ConnectionError as error:
             print(f"Error: {error} from send_java_data")
+            self.__send_func_alive = False
 
     def __get_local_data(self):
         """get data from java"""
+        self.__get_func_alive = True
         try:
             while True:
-                data = self._virtual_client.recv(client.MAX_LENGTH)
+                try:
+                    data = self._virtual_client.recv(client.MAX_LENGTH)
+                except TimeoutError:
+                    if not self.__send_func_alive:
+                        self.__get_func_alive = False
+                        break
+                    else:
+                        continue
+
+                if not self.__send_func_alive:
+                    self.__get_func_alive = False
+                    break
+
                 self._data_queue_1.put(data)
                 if debug:
                     if data:
                         print("[H]get_local_data: ", data)
         except ConnectionError as error:
             print(f"Error: {error} from get_local_data")
+            self.__get_func_alive = False
 
     def virtual_client_main(self):
         functions = [self.__send_java_data, self.__get_local_data]
 
         while True:
             self.__connect_mc_server()
+            self._virtual_client.settimeout(2)
 
             threads = [threading.Thread(target=func) for func in functions]
 
@@ -61,6 +112,7 @@ class HostClient(client.Client):
             for thd in threads:
                 thd.join()
 
+            self._virtual_client.settimeout(None)
             print("Virtual client is down!")
             input('type [ENTER] to restart> ')
             print("Virtual client restart...")
--- client/visitor_client.py
+++ client/visitor_client.py
@@ -1,10 +1,28 @@
 # the part which connected with Minecraft
 import socket
 import threading
+import time
 
 import client
 
 debug: bool = False
+
+_current_time = 0
+
+
+def ticker(time_break: float) -> bool:
+    global _current_time
+
+    if _current_time == 0:
+        _current_time = time.time()
+        return False
+    else:
+        current_time_break = time.time() - _current_time
+        if current_time_break >= time_break:
+            _current_time = time.time()
+            return True
+        else:
+            return False
 
 
 class VisitClient(client.Client):
@@ -15,6 +33,9 @@ class VisitClient(client.Client):
         self._port = virtual_server_port
         self._virtual_server: socket.socket
         self._mc_client: socket.socket
+
+        self._send_func_alive = False
+        self._get_func_alive = False
 
         self.__init_virtual_server()
 
@@ -37,32 +58,63 @@ class VisitClient(client.Client):
 
     def __send_java_data(self):
         """send data to java"""
+        self._send_func_alive = True
         try:
             while True:
-                data = self._data_queue_2.get()
+                try:
+                    data = self._data_queue_2.get(timeout=2)
+                except TimeoutError:
+                    if not self._get_func_alive:
+                        self._send_func_alive = False
+                        break
+                    else:
+                        if ticker(5):
+                            self._mc_client.send(b'')
+                        continue
+
+                if not self._get_func_alive:
+                    self._send_func_alive = False
+                    break
+
                 self._mc_client.sendall(data)
                 if debug:
                     if data:
                         print("[V]send_java_data: ", data)
         except ConnectionError as error:
             print(f"Error: {error} from send_java_data")
+            self._send_func_alive = False
 
     def __get_local_data(self):
         """get data from java"""
+        self._get_func_alive = True
         try:
             while True:
-                data = self._mc_client.recv(client.MAX_LENGTH)
+                try:
+                    data = self._mc_client.recv(client.MAX_LENGTH)
+                except TimeoutError:
+                    if not self._send_func_alive:
+                        self._get_func_alive = False
+                        break
+                    else:
+                        continue
+
+                if not self._send_func_alive:
+                    self._get_func_alive = False
+                    break
+
                 self._data_queue_1.put(data)
                 if debug:
                     if data:
                         print("[V]get_local_data: ", data)
         except ConnectionError as error:
             print(f"Error: {error} from get_local_data")
+            self._get_func_alive = False
 
     def virtual_server_main(self):
         functions = [self.__send_java_data, self.__get_local_data]
         while True:
             self.__connect_mc_client()
+            self._mc_client.settimeout(2)
 
             threads = [threading.Thread(target=func) for func in functions]
 
@@ -72,4 +124,5 @@ class VisitClient(client.Client):
             for thd in threads:
                 thd.join()
 
+            self._mc_client.settimeout(None)
             print("Virtual server is down, restart...")
