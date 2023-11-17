c0b678d1cd20d1179a0371c98700457eaf73b53d
Max <f233xdd@outlook.com>
2023-11-17 17:14:08 +0800

    update

---
+++ test/test.py
@@ -0,0 +1,9 @@
+class DataCreator:
+
+    def __init__(self, loop_data):
+        self.loop_data = loop_data
+        self.ans = []
+        self.res = []
+
+    def create_data(self):
+        pass
